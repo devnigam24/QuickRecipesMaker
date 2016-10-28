4ac23e816b4b6292a3ca8e4a2e328c62950e3581
Nimesh Ramesh Prabhu <nimesh5@csu.fullerton.edu>
2016-10-28 13:48:24 -0700

    front end

--- Front End/js/custom/init.js
+++ Front End/js/custom/init.js
@@ -9,3 +9,23 @@ $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal-trigger').leanModal();
   });
+$("#dropdowntimer").click(function(){
+    
+  var time1 = $('#dropdowntimer option:selected').text();
+    alert("clicked "+time1 );
+});
+
+$("#dropdowncalorie").click(function(){
+    alert("clicked.");
+});
+
+$('.chips').material_chip();
+  
+$('.chips-placeholder').material_chip({
+    placeholder: 'Enter a Ingredient',
+   
+  });
+  
+ var chip = {
+    tag: 'chip content',
+  };
